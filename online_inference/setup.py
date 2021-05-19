813859898afcd2385cda32903ca277aee8e601bc
= <d.svirchkov@qiwi.com>
2021-05-19 17:21:39 +0300

    hw #2 initial

---
+++ online_inference/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setup(
+    name="heart_disease",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Example of ml project - heart_disease",
+    author="spin to win",
+    install_requires=required,
+    license="MIT",
+)
